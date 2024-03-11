952557dbda09884dcc1bd2c66e2e86b154d08865
Wei Lai <wlai@liveperson.com>
2024-03-11 17:12:13 -0400

    update lp process

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const authConfig = {
   scope: "openid profile email", // Adjust scope as needed
 };
 
+let code = null;
+
 let accessToken = null;
 
 function login() {
+  window.localStorage.setItem("loginBy", "web");
   const codeVerifier = generate_code_verifier();
   const codeChallenge = generate_code_challenge(codeVerifier);
 
@@ -26,25 +29,26 @@ function login() {
       authConfig.scope
     )}&code_challenge=${codeChallenge}&state=${generatedState}` +
     "&code_challenge_method=S256";
-
+  console.log("auth URL = " + authorizationUrl);
   // Open the authorization URL in a new window or redirect the current window
   window.location.href = authorizationUrl;
 }
 
 function handleCallback() {
+  const initiatedBy = window.localStorage.getItem("loginBy");
   const queryParams = new URLSearchParams(window.location.search);
-  const code = queryParams.get("code");
+  code = queryParams.get("code");
 
   const codeVerifierValue = localStorage.getItem("savedCodeVerifierValue");
 
-  if (code) {
+  if (code && initiatedBy == "web") {
     // Exchange the code for an access token
     // in LP world, you will send the code and codeVerifer value in LP's getAuthenitcationJWT callback
     const tokenUrl =
       `${authConfig.tokenUrl}?client_id=${authConfig.clientId}&redirect_uri=${authConfig.redirectUri}` +
       `&grant_type=authorization_code&code=${code}&code_verifier=${codeVerifierValue}`;
 
-      console.log(tokenUrl);
+    console.log(tokenUrl);
     fetch(tokenUrl, {
       method: "POST",
       headers: {
@@ -55,9 +59,11 @@ function handleCallback() {
       .then((data) => {
         accessToken = data.access_token;
         console.log("Access Token:", accessToken);
-        document.getElementById('dynamicText').innerHTML = `Token = ${accessToken}`;
+        document.getElementById(
+          "dynamicText"
+        ).innerHTML = `Token = ${accessToken}`;
         // console.log("save access token to local storage " + accessToken);
-        localStorage.setItem('savedToken', accessToken);
+        localStorage.setItem("savedToken", accessToken);
       })
       .catch((error) =>
         console.error("Error exchanging code for token:", error)
@@ -81,41 +87,101 @@ function logout() {
 // Check for the authorization code in the URL when the page loads
 handleCallback();
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Hello from LP!');
-    var lpTag = window.lpTag || {};
-    lpTag.identities = [];
-    // Your additional JavaScript logic goes here
-    function identityFn(callback) {
-      console.log("identity function");
-      callback({
-        // all three are required
-        iss: "https://dev-24350261.okta.com",
-        acr: "loa1",
-        sub: "920001",
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hello from LP!");
+  var lpTag = window.lpTag || {};
+  lpTag.identities = [];
+  // Your additional JavaScript logic goes here
+  function identityFn(callback) {
+    console.log("identity function");
+    callback({
+      // all three are required
+      iss: "https://dev-24350261.okta.com",
+      acr: "loa1",
+      sub: "920001",
+    });
+  }
+
+  lpTag.identities.push(identityFn);
+
+  window.lpLoginUser = function (pkce) {
+    window.localStorage.setItem("loginBy", "lp");
+    const generatedState = random_string(32);
+    //const authURI = authConfig.authorizationUrl;
+    var cnf = {
+      redirect_uri: authConfig.redirectUri,
+      client_id: authConfig.clientId,
+      scope: authConfig.scope,
+      response_type: "code",
+      code_challenge: pkce,
+      code_challenge_method: "S256",
+      state: generatedState,
+    };
+
+    // Convert JSON to URL parameters
+    const urlParameters = Object.keys(cnf)
+      .map(
+        (key) =>
+          encodeURIComponent(key) + "=" + encodeURIComponent(cnf[key])
+      )
+      .join("&");
+
+    // Append parameters to a base URL
+    const baseUrl = authConfig.authorizationUrl;
+    const fullUrl = baseUrl + "?" + urlParameters;
+
+    console.log(fullUrl);
+    window.location.href = fullUrl;
+    
+  };
+
+  // Authentication JSMethod for LiveEngage
+  // https://l-p.atlassian.net/wiki/spaces/MAN/pages/178225162/Support+oAuth2.0+code+flow+with+PKCE
+  // LP generates the pkce code verifier and keeping it secret
+  window.lpGetAuthenticationToken = function (callback, pkce) {
+    console.log(
+      "inside lpGetAuthenticationToken! LP generated code verifier " + pkce
+    );
+
+    // cache pkce value to store code
+    window.localStorage.setItem("lppkce", pkce);
+
+
+
+    if (typeof code === "undefined" || code === null) {
+      console.log("code undefined");
+      window.lpLoginUser(pkce);
+    } else {
+      console.log("code exists " + code);
+
+      var payload = {
+        ssoKey: code,
+        redirect_uri: document.location.href,
+      };
+      // console.log('error: ' + error);
+      var error;
+      if (error !== "undefined") {
+        callback(payload, error);
+      } else {
+        callback(payload);
+      }
     }
 
-    lpTag.identities.push(identityFn);
+    // const authCode = localStorage.getItem("savedToken");
+    // const authCode2 = "WvMydGyvkUmz6TTTedTvWMG4QNzED5lVnIyrEnHodF";
 
-    // Authentication JSMethod for LiveEngage
-    window.lpGetAuthenticationToken = function (callback, pkce) {
-      console.log("inside lpGetAuthenticationToken! " + pkce);  
-      const authCode = localStorage.getItem('savedToken');
-      const authCode2 = "WvMydGyvkUmz6TTTedTvWMG4QNzED5lVnIyrEnHodF";
-      
-      console.log("call back with auth code " + authCode)
-      const REDIRECT_URI = "http://localhost:5500/index.html";
-      callback({ssoKey: authCode2, redirect_uri: REDIRECT_URI});
+    // console.log("call back with auth code " + authCode);
+    // const REDIRECT_URI = "http://localhost:5500/index.html";
+    // callback({ ssoKey: authCode2, redirect_uri: REDIRECT_URI });
     //   callback(authCode)
-      // const URL_PARAMS = new URLSearchParams(window.location.search);
-      // const code = URL_PARAMS.get('code');
+    // const URL_PARAMS = new URLSearchParams(window.location.search);
+    // const code = URL_PARAMS.get('code');
 
     //   // NOTE: this is the location of the code, if oauth ever decide to change it then it will break
     //   let code = "garbage";
     //   if(window.location.search.indexOf("code") > 0){
     //     code = window.location.search.split("&")[0].split("=")[1];
-    //   } 
+    //   }
     //   //- let uri = "http://localhost:3000";
     //   let uri = "http://localhost:3000/authorization-code/callback";
 
@@ -126,9 +192,6 @@ document.addEventListener('DOMContentLoaded', function() {
     //     ssoKey: code,
     //     redirect_uri: uri
     //   });
-      //- callback(code);
-    }
-
-
+    //- callback(code);
+  };
 });
-
